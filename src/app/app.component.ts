7edbef169dc5942bdfc9abf28d90b1145f31c153
beka zedginidze <58108808+beka-zedginidze@users.noreply.github.com>
2020-09-22 16:45:04 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  language: ['en', 'ka'];
+  
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'ge']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|ge/) ? browserLang : 'en');
+    console.log(translate.getLangs(),"language ---------------------------------------",
+    browserLang)
+  }
+
+
+
+
   @Input() books: any[];
   title = 'books-api';
   ret () {
