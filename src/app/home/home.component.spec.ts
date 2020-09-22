485cffb7b0b825abbce5dd2c5ffbb7f07fec379a
beka zedginidze <58108808+beka-zedginidze@users.noreply.github.com>
2020-09-22 16:41:22 +0400

    Update home.component.spec.ts

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,61 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { NavComponent } from '../nav/nav.component';
 
-import { HomeComponent } from './home.component';
+@Component({
+  selector: 'app-search',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  recomendedBooks;
+  onSearch1: boolean = true;
+  books;
+  srcBooks;
+  numberfavBooks: number = 0;
+  
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+  constructor(private data: DataService,) { }
+   
+  addBooksToFavorites(book) {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
+    const bookExistInCart = NavComponent.bookList.find(({title}) => title === book.volumeInfo.title);
+    console.log("works")
+    if (!bookExistInCart) {
+      NavComponent.bookList.push({...book, num: 1});
+      this.numberfavBooks += 1;
+      console.log(this.numberfavBooks, "number fav books")
+      console.log(NavComponent.bookList)
+      alert("Add to favorite")
+      return;
+    } 
+    bookExistInCart.num += 1;
+    
+  }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  OnSearch(s) {
+    this.srcBooks = this.data.SearchBooks(s)
+    .subscribe((data) => {
+      this.srcBooks  = data.items;
+      console.log('OnSearch data: ', this.srcBooks);
+      
+    });
+    this.onSearch1 = false;
+  }
+
+
+  ngOnInit() {
+    this.data.SearchBooks('The Lord Of The Rings')
+        .subscribe((data) => {
+          this.books  = data.items;
+          console.log('ngOnInit', data);
+        });
+        this.data.SearchBooks1('THE GUARDIANS')
+        .subscribe((data) => {
+          this.recomendedBooks  = data.items;
+          console.log('ngOnInit-recent', data);
+        });     
+  }
+
+}
